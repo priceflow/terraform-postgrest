d20841a1be99caff713b251d3797467e9369f0e8
Bryan Galvin <bryan@priceflow.com>
2019-01-07 11:00:19 -0800

    check in module

--- vars.tf
+++ vars.tf
@@ -55,12 +55,7 @@ variable "hosted_zone_id" {
 }
 
 variable "domain_name" {
-  description = "Name of postgrest subdomain"
-  default     = ""
-}
-
-variable "subdomain_name" {
-  description = "Name of postgrest subdomain"
+  description = "Name of postgrest top domain"
   default     = ""
 }
 
