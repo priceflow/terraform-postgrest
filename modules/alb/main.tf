b92d8f3e8d67c066cd335451b0497f0ceea571c5
Bryan Galvin <bryan@priceflow.com>
2019-01-16 10:18:37 -0800

    check in outputs

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -39,6 +39,7 @@ resource "aws_security_group_rule" "https_ingress" {
 module "access_logs" {
   source        = "git::https://github.com/cloudposse/terraform-aws-lb-s3-bucket.git?ref=tags/0.1.0"
   namespace     = "priceflow"
+  prefix        = ""
   name          = "${var.name}"
   stage         = "${var.stage}"
   tags          = "${var.tags}"
